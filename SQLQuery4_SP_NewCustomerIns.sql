6099853325dcbbacc01422524dbd378c4950ee48
Parbati Thapa <parbatithapamagar001@gmail.com>
2025-06-16 16:50:34 +0545

    Updated SpNewCustomerIns

--- SQLQuery4_SP_NewCustomerIns.sql
+++ SQLQuery4_SP_NewCustomerIns.sql
@@ -2,13 +2,62 @@ USE OrganizationInfo
 GO
 
 /**					NewCustomer Ins						**/
-CREATE PROCEDURE SpNewCustomerIns
-@json NVARCHAR(MAX) OUTPUT
+CREATE OR ALTER PROCEDURE SpNewCustomerIns
+@json NVARCHAR(MAX)
 AS
 BEGIN
 	BEGIN TRY
 		BEGIN TRANSACTION
+			CREATE TABLE #temp
+			(	
+				OrganizationId INT,
+				Status NVARCHAR(200),
+				Street NVARCHAR(200),
+				City NVARCHAR(200),
+				State NVARCHAR(200),
+				ZipCode NVARCHAR(200),
+				Country NVARCHAR(200),
+				PhoneNumber NVARCHAR(200),
+				Email NVARCHAR(200),
+				Website NVARCHAR(200),
+				UserPersonId INT
+			);
 
+			INSERT INTO #temp(OrganizationId, Status, Street, State, ZipCode, Country, PhoneNumber, Email, Website, UserPersonId)
+			SELECT OrganizationId, Status, Street, State, ZipCode, Country, PhoneNumber, Email, Website, UserPersonId
+			FROM OPENJSON(@json)
+			WITH
+			(
+				OrganizationId INT,
+				Status NVARCHAR(200),
+				Street NVARCHAR(200),
+				City NVARCHAR(200),
+				State NVARCHAR(200),
+				ZipCode NVARCHAR(200),
+				Country NVARCHAR(200),
+				PhoneNumber NVARCHAR(200),
+				Email NVARCHAR(200),
+				Website NVARCHAR(200),
+				UserPersonId INT	
+			)
+
+			DECLARE @insertNc TABLE
+			(
+				NewCustomerId INT,
+				OrganizationId INT,
+				Status NVARCHAR(200),
+				UserPersonId INT,
+				InsertDate DATE
+			)
+
+			INSERT INTO NewCustomer(OrganizationId, Status, UserPersonId, InsertDate)
+			OUTPUT INSERTED.* INTO @insertNc
+			SELECT OrganizationId, Status, UserPersonId, getdate()
+			FROM #temp
+
+			SELECT * FROM @insertNc;
+			DROP TABLE #temp;
+			
 		COMMIT TRANSACTION
 	END TRY
 	BEGIN CATCH
@@ -30,3 +79,59 @@ BEGIN
 		RAISERROR(@Error_Message, 16,1 );
 	END CATCH
 END
+
+DECLARE @json NVARCHAR(MAX)=
+N'
+[
+  {
+    "OrganizationName": "AllDIN Freight",
+    "Department": "Logistics",
+    "Parent": 101,
+    "Status": "Active",
+    "Street": "Kalanki",
+    "City": "Kathmandu",
+    "State": "Bagmati",
+    "ZipCode": "44602",
+    "Country": "Nepal",
+    "PhoneNumber": "+977-1-6677889",
+    "Email": "logistics@himalayanfreight.com",
+    "Website": "https://himalayanfreiht.com",
+    "UserPersonId": 5,
+    "InsertDate": "2025-06-15",
+    "OrganizationId": 124
+  },
+  {
+    "OrganizationName": "Global Agro Farms",
+    "Department": "Agriculture",
+    "Parent": 102,
+    "Status": "Active",
+    "Street": "Maitidevi",
+    "City": "Kathmandu",
+    "State": "Bagmati",
+    "ZipCode": "44601",
+    "Country": "Nepal",
+    "PhoneNumber": "+977-1-4433221",
+    "Email": "contact@globalagro.com",
+    "Website": "https://glbalagro.com",
+    "UserPersonId": 5,
+    "InsertDate": "2025-06-15",
+    "OrganizationId": 125
+  }
+]'
+
+EXEC SpNewCustomerIns @json
+PRINT @json;
+
+
+/**
+--		Created this table to check if the SpNewCustomerINs works or not
+CREATE TABLE NewCustomerCheck
+(
+	NewCustomerId INT IDENTITY(1,1),
+	OrganizationId INT,
+	Status NVARCHAR(200),
+	UserPersonId INT,
+	InsertDate DATE
+)
+
+**/
--- SQLQuery5_SP_CompanyIns.sql
+++ SQLQuery5_SP_CompanyIns.sql
@@ -2,13 +2,62 @@ USE OrganizationInfo
 GO
 
 /**					Company Ins						**/
-CREATE PROCEDURE SpCompanyIns
-@json NVARCHAR(MAX) OUTPUT
+CREATE OR ALTER PROCEDURE SpCompanyIns
+@json NVARCHAR(MAX)
 AS
 BEGIN
 	BEGIN TRY
 		BEGIN TRANSACTION
+			CREATE TABLE #temp
+			(	
+				OrganizationId INT,
+				CompanyName NVARCHAR(200),
+				Status NVARCHAR(200),
+				Street NVARCHAR(200),
+				City NVARCHAR(200),
+				State NVARCHAR(200),
+				ZipCode NVARCHAR(200),
+				Country NVARCHAR(200),
+				PhoneNumber NVARCHAR(200),
+				Email NVARCHAR(200),
+				Website NVARCHAR(200),
+				UserPersonId INT
+			);
 
+			INSERT INTO #temp(OrganizationId, CompanyName, Status, Street, City, State, ZipCode, Country, PhoneNumber, Email, Website, UserPersonId)
+			SELECT Organizationid, CompanyName, Status, Street, City, State, ZipCode, Country, PhoneNumber, Email, Website, UserPersonId
+			FROM OPENJSON(@json)
+			WITH
+			(
+				OrganizationId INT,
+				CompanyName NVARCHAR(200),
+				Status NVARCHAR(200),
+				Street NVARCHAR(200),
+				City NVARCHAR(200),
+				State NVARCHAR(200),
+				ZipCode NVARCHAR(200),
+				Country NVARCHAR(200),
+				PhoneNumber NVARCHAR(200),
+				Email NVARCHAR(200),
+				Website NVARCHAR(200),
+				UserPersonId INT
+			)
+			
+			DECLARE @InsertCompany TABLE 
+			(
+				CompanyId INT,
+				CompanyName NVARCHAR(200),
+				OrganizationId INT,
+				UserPersonId INT,
+				InsertDate DATE
+			)
+			INSERT INTO Company(CompanyName, OrganizationId, UserPersonId, InsertDate)
+			OUTPUT INSERTED.* INTO @InsertCompany
+			SELECT CompanyName, OrganizationId, UserPersonId, GETDATE()
+			FROM #temp
+
+			SELECT * FROM @InsertCompany;
+			DROP TABLE #temp;
 		COMMIT TRANSACTION
 	END TRY
 	BEGIN CATCH
@@ -19,7 +68,7 @@ BEGIN
 		DECLARE @Error_Message NVARCHAR(200);
 		DECLARE @Error_Line INT;
 		DECLARE @Error_Procedure NVARCHAR(200);
-
+		
 		SET @Error_Message  = ERROR_MESSAGE();
 		SET @Error_Line		= ERROR_LINE();
 		SET @Error_Procedure= ERROR_PROCEDURE();
@@ -30,3 +79,55 @@ BEGIN
 		RAISERROR(@Error_Message, 16,1 );
 	END CATCH
 END
+
+
+DECLARE @json NVARCHAR(MAX) = N'
+[
+  {
+    "OrganizationId": 104,
+    "CompanyName": "Everest Holdings Pvt. Ltd.",
+    "Status": "Active",
+    "Street": "Putalisadak",
+    "City": "Kathmandu",
+    "State": "Bagmati",
+    "ZipCode": "44600",
+    "Country": "Nepal",
+    "PhoneNumber": "+977-1-5555555",
+    "Email": "info@everestholdings.com",
+    "Website": "https://evholdings.com",
+    "UserPersonId": 7
+  },
+  {
+    "OrganizationId": 107,
+    "CompanyName": "Sagarmatha Agro Inc.",
+    "Status": "Active",
+    "Street": "Thapathali",
+    "City": "Kathmandu",
+    "State": "Bagmati",
+    "ZipCode": "44605",
+    "Country": "Nepal",
+    "PhoneNumber": "+977-1-4444444",
+    "Email": "contact@sagarmathaagro.com",
+    "Website": "https://sagthaagro.com",
+    "UserPersonId": 7
+  }
+]';
+
+EXEC SpCompanyIns @json;
+PRINT @json
+
+
+
+/**
+--		Created this table to check if the SpNewCustomerINs works or not
+
+CREATE TABLE CompanyCheck
+(
+	CompanyId INT IDENTITY(1,1),
+	CompanyName NVARCHAR(200),
+	OrganizationId INT,
+	UserPersonId INT,
+	InsertDate DATE,
+)
+
+**/
