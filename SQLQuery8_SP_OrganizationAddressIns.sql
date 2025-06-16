1d3236b0fb6ca65b81349ed7b5d77fdf584892fc
Parbati Thapa <parbatithapamagar001@gmail.com>
2025-06-16 18:09:33 +0545

    Updated SP

--- SQLQuery8_SP_OrganizationAddressIns.sql
+++ SQLQuery8_SP_OrganizationAddressIns.sql
@@ -2,13 +2,39 @@ USE OrganizationInfo
 GO
 
 /**					Organization Address Ins						**/
-CREATE PROCEDURE SpOrganizationAddressIns
+CREATE OR ALTER PROCEDURE SpOrganizationAddressIns
 @json NVARCHAR(MAX) OUTPUT
 AS
 BEGIN
 	BEGIN TRY
 		BEGIN TRANSACTION
+		CREATE TABLE #temp
+		(
+			OrganizationId INT,
+			AddressId INT
+		);
+		INSERT INTO #temp(OrganizationId, AddressId)
+		SELECT OrganizationId, AddressId
+		FROM OPENJSON(@json)
+		WITH
+		(
+			OrganizationId INT,
+			AddressId INT
+		);
+		DECLARE @insertOA TABLE
+		(
+		OrganizationAddressId INT,
+		OrganizationId INT,
+		AddressId INT
+		);
+		INSERT INTO OrganizationAddress(OrganizationId, AddressId)
+		OUTPUT INSERTED.* INTO @insertOA
+		SELECT OrganizationId, AddressId
+		FROM #temp;
 
+		SELECT * FROM @insertOA;
+
+		DROP TABLE #temp;
 		COMMIT TRANSACTION
 	END TRY
 	BEGIN CATCH
@@ -24,9 +50,50 @@ BEGIN
 		SET @Error_Line		= ERROR_LINE();
 		SET @Error_Procedure= ERROR_PROCEDURE();
 
-		PRINT 'Error line is:		' + @Error_Line;
+		PRINT 'Error line is:		' + CAST(@Error_Line AS NVARCHAR);
 		PRINT 'Error procedure is:  ' + ISNULL(@Error_Procedure, 'Unknown Procedure');
 		PRINT 'Error message is:	' + @Error_Message;
 		RAISERROR(@Error_Message, 16,1 );
 	END CATCH
 END
+
+
+
+
+DECLARE @json NVARCHAR(MAX)=
+N'
+[
+  {
+    "ContactId": 5,
+    "OrganizationId": 101,
+    "AddressId": 111,
+    "PhoneNumber": "+977-1-6677889",
+    "Email": "logistics@himalayanfreight.com",
+    "Website": "https://himalayanfreight.com",
+    "UserPersonId": 5
+  },
+  {
+    "ContactId": 6,
+    "OrganizationId": 111,
+    "AddressId": 112,
+    "PhoneNumber": "+977-1-4433221",
+    "Email": "contact@globalagro.com",
+    "Website": "https://globalagro.com",
+    "UserPersonId": 5
+  }
+]
+'
+EXEC SpOrganizationAddressIns @json
+print @json
+
+
+/**
+--		to check either SpOrganizationAddressIns works or not
+CREATE TABLE OrganizationAddressCheck
+(
+	OrganizationAddressId INT IDENTITY(201,1),
+	OrganizationId INT,
+	AddressId INT
+)
+
+**/
--- SQLQuery9_SP_OrganizationContactIns.sql
+++ SQLQuery9_SP_OrganizationContactIns.sql
@@ -2,13 +2,38 @@ USE OrganizationInfo
 GO
 
 /**					Company Ins						**/
-CREATE PROCEDURE SpCompanyIns
+CREATE OR ALTER PROCEDURE SpOrganizationContactIns
 @json NVARCHAR(MAX) OUTPUT
 AS
 BEGIN
 	BEGIN TRY
 		BEGIN TRANSACTION
+		CREATE TABLE #temp
+		(
+			OrganizationId INT,
+			ContactId INT
+		);
+		INSERT INTO #temp(OrganizationId, ContactId)
+		SELECT OrganizationId, ContactId
+		FROM OPENJSON(@json)
+		WITH
+		(
+			OrganizationId INT,
+			ContactId INT
+		);
+		DECLARE @insertOc TABLE
+		(
+			OrganizationContactId INT ,
+			OrganizationId INT,
+			ContactId INT
+		);
+		INSERT INTO OrganizationContactCheck(OrganizationId, ContactId)
+		OUTPUT INSERTED.* INTO @insertOc
+		SELECT OrganizationId, ContactId 
+		FROM #temp;
 
+		SELECT * FROM @insertOc;
+		DROP TABLE #temp;
 		COMMIT TRANSACTION
 	END TRY
 	BEGIN CATCH
@@ -24,9 +49,47 @@ BEGIN
 		SET @Error_Line		= ERROR_LINE();
 		SET @Error_Procedure= ERROR_PROCEDURE();
 
-		PRINT 'Error line is:		' + @Error_Line;
+		PRINT 'Error line is:		' + CAST(@Error_Line AS NVARCHAR);
 		PRINT 'Error procedure is:  ' + ISNULL(@Error_Procedure, 'Unknown Procedure');
 		PRINT 'Error message is:	' + @Error_Message;
 		RAISERROR(@Error_Message, 16,1 );
 	END CATCH
 END
+
+
+DECLARE @json NVARCHAR(MAX)=
+N'
+[
+  {
+    "ContactId": 5,
+    "OrganizationId": 101,
+    "AddressId": 111,
+    "PhoneNumber": "+977-1-6677889",
+    "Email": "logistics@himalayanfreight.com",
+    "Website": "https://himalayanfreight.com",
+    "UserPersonId": 5
+  },
+  {
+    "ContactId": 6,
+    "OrganizationId": 111,
+    "AddressId": 112,
+    "PhoneNumber": "+977-1-4433221",
+    "Email": "contact@globalagro.com",
+    "Website": "https://globalagro.com",
+    "UserPersonId": 5
+  }
+]
+'
+EXEC SpOrganizationContactIns @json
+print @json
+
+/**
+
+CREATE TABLE OrganizationContactCheck
+(
+	OrganizationContactId INT IDENTITY(111,1),
+	OrganizationId INT,
+	ContactId INT
+)
+
+**/
